14cb434b7d4ddf0edbefb677fe17779c68efd42d
Narayan Singh Solnalki <hritiksingh580@gmail.com>
2020-05-20 16:42:57 +0530

    Adding Data

--- lib/screens/chats_screen.dart
+++ lib/screens/chats_screen.dart
@@ -5,24 +5,35 @@ class ChatsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: StreamBuilder(
-          stream: Firestore.instance
+      body: StreamBuilder(
+        stream: Firestore.instance
+            .collection('chats/NODlH95WgbWZccmAsYr9/message')
+            .snapshots(),
+        builder: (ctx, streamSnapShot) {
+          if (streamSnapShot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          final documents = streamSnapShot.data.documents;
+          return ListView.builder(
+              itemCount: documents.length,
+              itemBuilder: (ctx, index) => Container(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(documents[index]['test']),
+                  ));
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Firestore.instance
               .collection('chats/NODlH95WgbWZccmAsYr9/message')
-              .snapshots(),
-          builder: (ctx, streamSnapShot) {
-            if (streamSnapShot.connectionState == ConnectionState.waiting) {
-              return Center(child: CircularProgressIndicator());
-            }
-            final document = streamSnapShot.data.document;
-            return ListView.builder(
-                itemCount: document.length,
-                itemBuilder: (ctx, index) => Container(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(document[index]['test']),
-                    ));
-          },
-        ),
-        floatingActionButton:
-            FloatingActionButton(child: Icon(Icons.add), onPressed: () {}));
+              .add(
+            {
+              'test': 'Adding by button!',
+            },
+          );
+        },
+      ),
+    );
   }
 }
